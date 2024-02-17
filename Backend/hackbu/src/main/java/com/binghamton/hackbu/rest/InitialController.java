8d0276360891d89764f17b183bf9a7c18027c278
Prem Bhajaj <prembhajaj@gmail.com>
2024-02-17 16:34:18 -0500

    modified rest controller

--- Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/InitialController.java
+++ Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/InitialController.java
@@ -1,9 +1,11 @@
 package com.binghamton.hackbu.rest;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class InitialController {
 	@GetMapping("/")
 	public String sayHello() {

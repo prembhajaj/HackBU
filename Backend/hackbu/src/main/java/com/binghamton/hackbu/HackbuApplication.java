90ac886d27d7f80f8091dbbe8692c0753fd2fe54
Prem Bhajaj <prembhajaj@gmail.com>
2024-02-17 16:06:54 -0500

    added rest controller

--- Backend/hackbu/src/main/java/com/binghamton/hackbu/HackbuApplication.java
+++ Backend/hackbu/src/main/java/com/binghamton/hackbu/HackbuApplication.java
@@ -3,11 +3,13 @@ package com.binghamton.hackbu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.binghamton.hackbu.rest.InitialController;
+
 @SpringBootApplication
 public class HackbuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HackbuApplication.class, args);
+		SpringApplication.run(InitialController.class, args);
 	}
 
 }

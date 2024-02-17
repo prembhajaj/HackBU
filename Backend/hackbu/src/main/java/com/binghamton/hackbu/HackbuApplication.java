d9d68f8641791e6985422b359f0b7b5c9f2ebee2
Prem Bhajaj <prembhajaj@gmail.com>
2024-02-17 15:38:45 -0500

    Added spring starter project

---
+++ Backend/hackbu/src/main/java/com/binghamton/hackbu/HackbuApplication.java
@@ -0,0 +1,13 @@
+package com.binghamton.hackbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackbuApplication.class, args);
+	}
+
+}
---
+++ Backend/hackbu/src/test/java/com/binghamton/hackbu/HackbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghamton.hackbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

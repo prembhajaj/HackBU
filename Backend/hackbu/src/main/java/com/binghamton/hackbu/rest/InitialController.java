d35d442b3e55767f1a9f4b516fb0c70c685a87cd
Prem Bhajaj <prembhajaj@gmail.com>
2024-02-18 02:38:02 -0500

    added cors for backend

--- Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/InitialController.java
+++ Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/InitialController.java
@@ -1,11 +1,14 @@
 package com.binghamton.hackbu.rest;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @EnableAutoConfiguration
+@CrossOrigin(origins = "*")
 public class InitialController {
 	@GetMapping("/")
 	public String sayHello() {
--- Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/PlantUMLController.java
+++ Backend/hackbu/src/main/java/com/binghamton/hackbu/rest/PlantUMLController.java
@@ -4,6 +4,7 @@ import com.binghamton.hackbu.java2json.JavaToPlantUML;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,13 @@ import java.io.StringReader;
 @EnableAutoConfiguration
 public class PlantUMLController {
 
+	
     @PostMapping("/generate-uml")
+    @CrossOrigin(origins = "*")
     public String generateUML(@RequestBody String javaCode) {
         try {
             // Parse Java code
+        	System.out.println(javaCode);
             CompilationUnit cu = StaticJavaParser.parse(new StringReader(javaCode));
 
             // Visit and generate Plant UML text
@@ -26,6 +30,7 @@ public class PlantUMLController {
             String plantUMLText = generator.getPlantUMLText();
 
             // Return Plant UML text
+            System.out.println(plantUMLText);
             return plantUMLText;
         } catch (Exception e) {
             return "Error parsing Java code: " + e.getMessage();
